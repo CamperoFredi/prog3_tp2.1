f5e31856af96a4231d303414946c9a4b4d47208f
Fredi Campero <fredi.campero@zentricx.com>
2024-05-31 21:55:36 -0300

    desarrollo actividad 1

--- ejercicio1/sensors.js
+++ ejercicio1/sensors.js
@@ -1,5 +1,22 @@
-class Sensor {}
+// Definir una clase Sensor en el archivo sensors.js que permita representar un sensor.
+class Sensor {
+    constructor(id, name, type, value, unit, updated_at) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.value = value;
+        this.unit = unit;
+        this.updated_at = updated_at;
+    }
 
+    // Implementar la propiedad computada updateValue mediante un setter que permita actualizar el valor del sensor y la fecha de actualización.
+    set updateValue(newValue) {
+        this.value = newValue;
+        this.updated_at = new Date().toISOString();
+    }
+}
+
+// Clase SensorManager que maneja los sensores
 class SensorManager {
     constructor() {
         this.sensors = [];
@@ -13,14 +30,15 @@ class SensorManager {
         const sensor = this.sensors.find((sensor) => sensor.id === id);
         if (sensor) {
             let newValue;
+            // Los únicos valores para type permitidos son temperature, humidity y pressure.
             switch (sensor.type) {
-                case "temperatura": // Rango de -30 a 50 grados Celsius
+                case "temperature": // Rango de -30 a 50 grados Celsius
                     newValue = (Math.random() * 80 - 30).toFixed(2);
                     break;
-                case "humedad": // Rango de 0 a 100%
+                case "humidity": // Rango de 0 a 100%
                     newValue = (Math.random() * 100).toFixed(2);
                     break;
-                case "presion": // Rango de 960 a 1040 hPa (hectopascales o milibares)
+                case "pressure": // Rango de 960 a 1040 hPa (hectopascales o milibares)
                     newValue = (Math.random() * 80 + 960).toFixed(2);
                     break;
                 default: // Valor por defecto si el tipo es desconocido
@@ -33,7 +51,26 @@ class SensorManager {
         }
     }
 
-    async loadSensors(url) {}
+    // Para la clase SensorManager, la cual se encarga de gestionar los sensores mediante un arreglo, se solicita implementar el método loadSensors que se encargue de cargar los sensores desde el archivo sensors.json.
+    // El método debe ser asíncrono
+    async loadSensors(url) {
+        try {
+            // puede utilizar fetch o XMLHttpRequest. Pueden emplear async/await o promesas.
+            const response = await fetch(url);
+            const sensorData = await response.json();
+            sensorData.forEach(sensorInfo => {
+                const { id, name, type, value, unit, updated_at } = sensorInfo;
+                // Los únicos valores para type permitidos son temperature, humidity y pressure.
+                if (type == "temperature" || type == "humidity" || type == "pressure"){
+                    const sensor = new Sensor(id, name, type, value, unit, updated_at);
+                    this.addSensor(sensor);
+                }
+            });
+            this.render();
+        } catch (error) {
+            console.error('Ocurrio un error al cargar los sensores:', error);
+        }
+    }
 
     render() {
         const container = document.getElementById("sensor-container");
